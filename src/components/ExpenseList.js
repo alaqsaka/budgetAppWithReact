a252f42799796880b132ad13f8c0df0cd0ed8cd6
alaqsaka <aqsaabidin15@gmail.com>
2021-03-21 18:00:39 +0700

    Adding some components.

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ExpenseList = () => {
+  const expenses = [
+    {
+      id: 1,
+      name: "Shopping",
+      cost: 50000,
+    },
+    {
+      id: 2,
+      name: "Books",
+      cost: 99000,
+    },
+    {
+      id: 3,
+      name: "Coffee",
+      cost: 18000,
+    },
+    {
+      id: 4,
+      name: "Nasik Kulit Syurgaa",
+      cost: 20000,
+    },
+    {
+      id: 5,
+      name: "Coffee 2",
+      cost: 20000,
+    },
+  ];
+};
